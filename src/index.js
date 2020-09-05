e02a3f81075db7bcfdf2c0fcf08a9080941b047e
Dmitriy Evstigneyev <evstigneeff@gmail.com>
2020-09-04 21:44:32 -0400

    v1.1.0

--- src/index.js
+++ src/index.js
@@ -20,11 +20,9 @@ bot.command("exit", (ctx) => ctx.reply("Welcome to Keep Network Telegram Bot! \n
     "\n/rb    To get some Random Beacon statistics \n/ecdsa    To get some ECDSA Statistics"))
 bot.command("rb", (ctx) => ctx.reply("The options are as follows: \n\nRun rb 'your address' to get your ECDSA Node statistics \n OR" +
     "\n/rb_node_number    Cumulative number of Random Beacon Nodes" +
-    // "\n/rb_node_number    Cumulative number of Random Beacon Nodes \n/rb_node_list    Full list of Random Beacon Nodes" +
     "\n\n/keep_stake    Total number of staked KEEP \n/keep_slash    Total number of slashed KEEP \n\n/exit    Return to main menu \n\nMORE OPTIONS TO COME!"))
 bot.command("ecdsa", (ctx) => ctx.reply("The options are as follows: \n\nRun ecdsa 'your address' to get your ECDSA Node statistics \n OR" +
     "\n/ecdsa_node_number    Cumulative number of ECDSA Nodes" +
-    // "\n/ecdsa_node_number    Cumulative number of ECDSA Nodes \n/ecdsa_node_list    Full list of ECDSA Nodes" +
     "\n\nRun eth 'your address' to get contribution of your node to ETH bonding pool\n OR \n/eth_bond    Total number of ETH available for bonding " + 
     "\n\n/exit    Return to main menu \n\nMORE OPTIONS TO COME!"))
 
@@ -38,32 +36,6 @@ bot.command("ecdsa_node_number", async (ctx) => {
     const resp = await getECDSA()
     ctx.reply(resp.length)
 })
-// // Get a list of Random Beacon Nodes 
-// bot.command("rb_node_list", async (ctx) => {
-//     const resp = await getRB()
-//     if (resp.length === 0) {
-//         ctx.reply("No Random Beacon Nodes found")
-//     } else {
-//         const str = resp.join("\n")
-//         fs.writeFile(`${fileDir}/rb.csv`, str, (err) => {
-//             if (err) throw err
-//         })
-//     }
-//     ctx.replyWithDocument({ source: `${fileDir}/rb.csv` })
-// })
-// // Get a list of ECDSA Nodes 
-// bot.command("ecdsa_node_list", async (ctx) => {
-//     const resp = await getECDSA()
-//     if (resp.length === 0) {
-//         ctx.reply("No ECDSA Nodes found")
-//     } else {
-//         const str = resp.join("\n")
-//         fs.writeFile(`${fileDir}/ecdsa.csv`, str, (err) => {
-//             if (err) throw err
-//         })
-//     }
-//     ctx.replyWithDocument({ source: `${fileDir}/ecdsa.csv` })
-// })
 // Get a list of transactions of your Random Beacon Node
 bot.hears(/rb (.+)/i, async (ctx) => {
     const resp = await getYourRB(ctx.match[1])
